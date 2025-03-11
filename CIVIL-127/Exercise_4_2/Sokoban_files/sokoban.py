b0a96e68b9822e2080a6985659fa08adcbd8bad9
Corminbo <teho.corminboeuf@epfl.ch>
2025-03-11 15:37:46 +0100

    Aucune modification apportée au code

--- CIVIL-127/Exercise_4_2/Sokoban_files/sokoban.py
+++ CIVIL-127/Exercise_4_2/Sokoban_files/sokoban.py
@@ -2,6 +2,7 @@ import os
 from sokoban_controller import SokobanController
 
 # use os.path.join for portable code (so it works on Mac/Windows/Linux)
-path = os.path.join("levels", "level1.xsb")
+path = os.path.join("/Users/tcormin/Desktop/Travail/EPFL/BA2/Programming and software development for engieers/civil127-2025/CIVIL-127/Exercise_4_2/Sokoban_files/levels"
+"", "/Users/tcormin/Desktop/Travail/EPFL/BA2/Programming and software development for engieers/civil127-2025/CIVIL-127/Exercise_4_2/Sokoban_files/levels/level1.xsb.txt")
 sokoban = SokobanController(path)
 sokoban.game_loop()
