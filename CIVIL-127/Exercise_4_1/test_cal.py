534ef589f272de024688ae1376bafb72da298c71
Corminbo <teho.corminboeuf@epfl.ch>
2025-03-11 14:50:16 +0100

    Mise à jour de test_cal.py avec les tests pour la classe Cal

--- CIVIL-127/Exercise_4_1/test_cal.py
+++ CIVIL-127/Exercise_4_1/test_cal.py
@@ -5,3 +5,15 @@ c.year(2025)
 c.month("jan")
 c.week_start("sun")
 c.print()
+
+c = Cal()
+c.month("feb")
+c.year(2025)
+c.print()
+
+c = Cal()
+c.month("aug")
+c.year(2025)
+c.week_start("thu")
+c.print()
+
